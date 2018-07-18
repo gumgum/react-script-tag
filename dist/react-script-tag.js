022783dc0969ddaed818977f6b5cd9b614596cd0
Jose Santos <josemsantos1994@gmail.com>
2018-07-18 00:04:59 -0700

    Add .gitignore file

--- dist/react-script-tag.js
+++
@@ -1,2 +0,0 @@
-var t,e=function(t){function e(){t.call(this),this.state={}}return t&&(e.__proto__=t),(e.prototype=Object.create(t&&t.prototype)).constructor=e,e.prototype.render=function(){return h("h1",null,"Hello World")},e}(((t=require("react"))&&"object"==typeof t&&"default"in t?t.default:t).Component);module.exports=e;
-//# sourceMappingURL=react-script-tag.js.map
