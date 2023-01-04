f41ea51e03ec8bf8e7c6e1e1ae5d5414339f4cae
Ziping Liu <dev@ziping.life>
2023-01-03 21:01:03 -0600

    chore: prettier

--- src/ScriptTag/index.tsx
+++ src/ScriptTag/index.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
 
 /** @class ScriptLoader
  * @description This a react component is intended to be a drop-in replacement for the <script> html native tag. After you add it in any location of your react-app, the component will take care on appending, the corresponding script tag to your app's document. It supports all the native attributes as well.
  * @example ```<ScriptLoader src="https://www.google.com/recaptcha/api.js" />```
-*/
-export default class ScriptTag extends React.Component<ScriptLoaderProps, {
-    delayMs: number;
-    src: string;
-    timeout: NodeJS.Timeout | null;
-    render: JSX.Element | null;
-    id: string | null;
-    renderScript: boolean;
-    updated: boolean;
-}> {
- 
-
+ */
+export default class ScriptTag extends React.Component<
+    ScriptLoaderProps,
+    {
+        delayMs: number;
+        src: string;
+        timeout: NodeJS.Timeout | null;
+        render: JSX.Element | null;
+        id: string | null;
+        renderScript: boolean;
+        updated: boolean;
+    }
+> {
     constructor(props: ScriptLoaderProps) {
-
-
         super(props);
 
         this.state = {
@@ -27,7 +26,7 @@ export default class ScriptTag extends React.Component<ScriptLoaderProps, {
             render: props.render || null,
             id: props.id || null,
             renderScript: props.renderScript || false,
-            updated: false
+            updated: false,
         };
 
         this.componentDidMount = this.componentDidMount.bind(this);
@@ -39,201 +38,262 @@ export default class ScriptTag extends React.Component<ScriptLoaderProps, {
         this.onLoad = this.onLoad.bind(this);
         this.onError = this.onError.bind(this);
         this.onSuccess = this.onSuccess.bind(this);
-
     }
 
     componentDidMount() {
-        this.log('componentDidMount started', 'color: #ff0000; font-weight: bold;', 2);
+        this.log(
+            "componentDidMount started",
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
 
         this.setState({
-            timeout: setTimeout(this._appendScript, this.state.delayMs)
+            timeout: setTimeout(this._appendScript, this.state.delayMs),
         });
 
-        this.log('componentDidMount exiting', 'color: #ff0000; font-weight: bold;', 2);
-
+        this.log(
+            "componentDidMount exiting",
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
     }
 
     log(msg: string, style?: string, indent?: number) {
-        
         if (this.props.debug) {
-            const indentStr = indent ? ' '.repeat(indent) : '';
-            console.log(`%c ScriptLoader debug {src: "${this.state.src}"}`, style || `color: #00ff00; font-weight: bold;`);
+            const indentStr = indent ? " ".repeat(indent) : "";
+            console.log(
+                `%c ScriptLoader debug {src: "${this.state.src}"}`,
+                style || `color: #00ff00; font-weight: bold;`
+            );
 
-            console.log(`%c ${indentStr} ${msg}`, style || `color: #00ff00; font-weight: bold;`);
+            console.log(
+                `%c ${indentStr} ${msg}`,
+                style || `color: #00ff00; font-weight: bold;`
+            );
         }
     }
 
     componentDidUpdate() {
-        this.log('componentDidUpdate started', 'color: #ff0000; font-weight: bold;', 2);
-        if(this.state.updated == false) {
+        this.log(
+            "componentDidUpdate started",
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
+        if (this.state.updated == false) {
             this.setState({
-                updated: true
+                updated: true,
             });
             this.onSuccess();
         }
-        this.log('componentDidUpdate exiting', 'color: #ff0000; font-weight: bold;', 2);
+        this.log(
+            "componentDidUpdate exiting",
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
     }
     componentWillUnmount() {
-        this.log('componentWillUnmount started', 'color: #ff0000; font-weight: bold;', 2);
+        this.log(
+            "componentWillUnmount started",
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
         if (this.state.timeout) {
-            this.log('Clearing Timeout...', 'color: #ff0000; font-weight: medium;', 4);
+            this.log(
+                "Clearing Timeout...",
+                "color: #ff0000; font-weight: medium;",
+                4
+            );
             clearTimeout(this.state.timeout);
-        };
-        this.log('componentWillUnmount exiting', 'color: #ff0000; font-weight: bold;', 2);
-
+        }
+        this.log(
+            "componentWillUnmount exiting",
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
     }
 
     _appendScript = () => {
+        this.log(
+            "_appendScript started",
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
 
-        this.log('_appendScript started', 'color: #ff0000; font-weight: bold;', 2);
+        const { delayMs, src, id } = this.state;
 
-        const {
-            delayMs,
-            src,
-            id
-        } = this.state;
-
+        const script = document.createElement("script");
+        script.src = src;
      
 
-        const script = document.createElement('script');
-        script.src = src;
+        if(this.state.id) {
+            script.id = this.state.id;
+        }
+
 
         const scriptPropKeys = Object.keys(this.state);
-
-        const otherPropsKeys = Object.keys(this.props).filter((prop) => !scriptPropKeys.includes(prop));
+        const otherPropsKeys = Object.keys(this.props).filter(
+            (prop) => !scriptPropKeys.includes(prop)
+        );
+        script.setAttribute("data-delayMs", delayMs.toString());
 
         // Add custom attributes
-        if(otherPropsKeys.length > 0) {
-
-        
-        for (const [attr, value] of Object.entries(this.props)) {
-
-            if (scriptPropKeys.includes(attr)) {
-                continue;
+        if (otherPropsKeys.length > 0) {
+            for (const [attr, value] of Object.entries(this.props)) {
+                if (scriptPropKeys.includes(attr)) {
+                    continue;
+                }
+                script.setAttribute(attr, value as any);
             }
-            script.setAttribute(attr, value as any);
         }
-    }
 
-        script.setAttribute('data-delayMs', delayMs.toString());
-
-        if(id) {
-            script.setAttribute('id', id);
-        }
-        
         script.onload = this.onLoad;
         script.onerror = this.onError;
         document.body.appendChild(script);
 
         this.onCreate();
-        
 
-        this.log('_appendScript exiting', 'color: #ff0000; font-weight: bold;', 2);
-
+        this.log(
+            "_appendScript exiting",
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
     };
 
-    onError (e?: Error | Event | string) {
-        const {
-   
-            onError
-        
-        } = this.props;
-        this.log(`onerror started... for { src: "${this.state.src}" }`, 'color: #ff0000; font-weight: bold;', 2);
-        if(onError) {
+    onError(e?: Error | Event | string) {
+        const { onError } = this.props;
+        this.log(
+            `onerror started... for { src: "${this.state.src}" }`,
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
+        if (onError) {
             onError(e);
         }
-        this.log(`onerror exiting... for { src: "${this.state.src}" }`, 'color: #ff0000; font-weight: bold;', 2);
+        this.log(
+            `onerror exiting... for { src: "${this.state.src}" }`,
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
     }
 
-    onSuccess (e?: Error | Event | string) {
-        const {
-   
-            onSuccess
-        
-        } = this.props;
-        this.log(`onsuccess started... for { src: "${this.state.src}" }`, 'color: #ff0000; font-weight: bold;', 2);
-        if(onSuccess) {
+    onSuccess(e?: Error | Event | string) {
+        const { onSuccess } = this.props;
+        this.log(
+            `onsuccess started... for { src: "${this.state.src}" }`,
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
+        if (onSuccess) {
             onSuccess();
         }
-        this.log(`onsuccess exiting... for { src: "${this.state.src}" }`, 'color: #ff0000; font-weight: bold;', 2);
+        this.log(
+            `onsuccess exiting... for { src: "${this.state.src}" }`,
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
     }
 
-    onLoad (e?:  Event | undefined) {
+    onLoad(e?: Event | undefined) {
         const customOnLoad = this.props.onLoad as (e?: Event) => void;
-        this.log(`onload started... for { src: "${this.state.src}" }`, 'color: #ff0000; font-weight: bold;', 2);
+        this.log(
+            `onload started... for { src: "${this.state.src}" }`,
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
 
-        if(e) {
-            this.log(`onload event: ${JSON.stringify(e, null, 2)}`, 'color: #ff0000; font-weight: bold;', 4)
-         }
-        if(customOnLoad) {
+        if (e) {
+            this.log(
+                `onload event: ${JSON.stringify(e, null, 2)}`,
+                "color: #ff0000; font-weight: bold;",
+                4
+            );
+        }
+        if (customOnLoad) {
             customOnLoad(e);
         }
-        this.log(`onload exiting... for { src: "${this.state.src}" }`, 'color: #ff0000; font-weight: bold;', 2);
+        this.log(
+            `onload exiting... for { src: "${this.state.src}" }`,
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
     }
 
-    onCreate (e?:  Event) {
-        const {
-   
-            onLoad
-        
-        } = this.props;
-        this.log(`oncreate started... for { src: "${this.state.src}" }`, 'color: #ff0000; font-weight: bold;', 2);
+    onCreate(e?: Event) {
+        const { onLoad } = this.props;
+        this.log(
+            `oncreate started... for { src: "${this.state.src}" }`,
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
 
-        if(e) {
-           this.log(`oncreate event: ${JSON.stringify(e, null, 2)}`, 'color: #ff0000; font-weight: bold;', 4)
+        if (e) {
+            this.log(
+                `oncreate event: ${JSON.stringify(e, null, 2)}`,
+                "color: #ff0000; font-weight: bold;",
+                4
+            );
         }
 
-        if(onLoad) {
+        if (onLoad) {
             onLoad(e);
         }
-        this.log(`oncreate exiting... for { src: "${this.state.src}" }`, 'color: #ff0000; font-weight: bold;', 2);
+        this.log(
+            `oncreate exiting... for { src: "${this.state.src}" }`,
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
     }
 
     render() {
-
-        const {
-            updated,
-            render
-        }   = this.state;
-        this.log(`render called, { src: ${this.state.src}}`, 'color: #ff0000; font-weight: bold;', 2);
+        const { updated, render } = this.state;
+        this.log(
+            `render called, { src: ${this.state.src}}`,
+            "color: #ff0000; font-weight: bold;",
+            2
+        );
 
         return updated && render ? render : null;
-
     }
 }
 
 /** @type ScriptLoaderProps This is the type declaration for props that can be passed to the ScriptLoader component, `import { ScriptLoaderProps } from 'react-script-loader-18'`
-     * @param src The source of the script to be loaded, e.g. 'https://www.google.com/recaptcha/api.js'
-     * @param delayMS Artifically adds a delay in milliseconds after the component mounts, but before the script tag is appended to the document. Useful for scripts that are not necessary early on, and may conflict on the browser's request-limit.
-     * @param onCreate A callback function that is called just right after the script tag has been appended to the document.
-     * @param onLoad This function is called after the script has been successfully loaded.
-     * @param onError This function is called if the script fails to load.
-     * @param onSuccess This function is called if the script is loaded successfully.
-     * @param render This is an optional JSX.Element that renders.
-     * @param debug This is a boolean flag that enables debug mode. It will log to the console the events that are triggered.
-     * @example ``
-     * import { ScriptLoaderProps } from 'react-script-loader-18'; 
-     * const props: ScriptLoaderProps = 
-     * { 
-     *   src: 'https://www.google.com/recaptcha/api.js', 
-     *   delayMs: 1000, onCreate: () => { console.log('Script created!') }, 
-     *   onLoad: (e: Event) => { console.log('Script loaded!') }, 
-     *   onError: (e: Event | string) => { console.log('Script failed to load!') }, 
-     *   onSuccess: () => { console.log('Script loaded successfully!') }, 
-     *   debug: true, 
-     *   id: JSX.Element tag id,
-     *   render: <><style> {`h3:hover {visibility: visible;}`} </style>
-     *  <h3 style={{visibility:hidden}}>Script loaded! AMAZON LEGAL, 
-     *  <a target='_blank' style={{textDecoration: none}} href='https://liu.academy/stakeholder/letter'>WHERE ARE MY WAGES
-     *  </a> </h3> </>
-     * }
-     * ``
-     */
+ * @param src The source of the script to be loaded, e.g. 'https://www.google.com/recaptcha/api.js'
+ * @param delayMS Artifically adds a delay in milliseconds after the component mounts, but before the script tag is appended to the document. Useful for scripts that are not necessary early on, and may conflict on the browser's request-limit.
+ * @param onCreate A callback function that is called just right after the script tag has been appended to the document.
+ * @param onLoad This function is called after the script has been successfully loaded.
+ * @param onError This function is called if the script fails to load.
+ * @param onSuccess This function is called if the script is loaded successfully.
+ * @param render This is an optional JSX.Element that renders.
+ * @param debug This is a boolean flag that enables debug mode. It will log to the console the events that are triggered.
+ * @example ``
+ * import { ScriptLoaderProps } from 'react-script-loader-18';
+ * const props: ScriptLoaderProps =
+ * {
+ *   src: 'https://www.google.com/recaptcha/api.js',
+ *   delayMs: 1000, onCreate: () => { console.log('Script created!') },
+ *   onLoad: (e: Event) => { console.log('Script loaded!') },
+ *   onError: (e: Event | string) => { console.log('Script failed to load!') },
+ *   onSuccess: () => { console.log('Script loaded successfully!') },
+ *   debug: true,
+ *   id: JSX.Element tag id,
+ *   render: <><style> {`h3:hover {visibility: visible;}`} </style>
+ *  <h3 style={{visibility:hidden}}>Script loaded! AMAZON LEGAL,
+ *  <a target='_blank' style={{textDecoration: none}} href='https://liu.academy/stakeholder/letter'>WHERE ARE MY WAGES
+ *  </a> </h3> </>
+ * }
+ * ``
+ */
 export type ScriptLoaderProps = {
     delayMs?: number;
     id?: string;
     onCreate?: (event?: Event) => void;
-    onError?: (event?: string | Event | Error, source?: string, lineno?: number, colno?: number, error?: Error) => void;
+    onError?: (
+        event?: string | Event | Error,
+        source?: string,
+        lineno?: number,
+        colno?: number,
+        error?: Error
+    ) => void;
     onSuccess?: () => void;
     onLoad?: (event?: Event) => void;
     debug?: boolean;
@@ -241,10 +301,4 @@ export type ScriptLoaderProps = {
     renderScript?: boolean;
     src: string;
     [key: string]: any;
-}
-
-
-
-
-
-
+};
--- src/__tests__/ScriptTag.test.tsx
+++ src/__tests__/ScriptTag.test.tsx
@@ -1,47 +1,46 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
 
-import ScriptTag  from '../ScriptTag';
+import ScriptTag from "../ScriptTag";
 import { ScriptLoaderProps } from "../ScriptTag";
 
-
 describe("<ScriptTag />", () => {
-  test("should display script tag element, as well as a custom render jsx element", async () => {
-    const {findByTestId} =  renderScriptTag();
-    const scriptTag = await findByTestId('script-tag-generated-tag');
-    const customRender = await findByTestId('custom-render');
-    expect(scriptTag).toBeDefined();
-    expect(customRender).toBeDefined();
+    test("should display script tag element, as well as a custom render jsx element", async () => {
+        const { findByTestId } = renderScriptTag();
+        const scriptTag = await findByTestId("script-tag-generated-tag");
+        const customRender = await findByTestId("custom-render");
+        expect(scriptTag).toBeDefined();
+        expect(customRender).toBeDefined();
 
-
-    const hisHonorableForm = await findByTestId('his-honorable-form');
-    expect(hisHonorableForm).toHaveFormValues({
-        "honorable-input-username": "Edward Falesetien"
-    })
-  });
+        const hisHonorableForm = await findByTestId("his-honorable-form");
+        expect(hisHonorableForm).toHaveFormValues({
+            "honorable-input-username": "Edward Falesetien",
+        });
+    });
 });
 
-
-function renderScriptTag(){
-  const props: ScriptLoaderProps = {
-      src: 'https://ercaws.com/time',
-      debug: true,
-      renderScript: true,
-     'data-testid': 'script-tag-generated-tag',
-      render: <div data-testid="custom-render">
+function renderScriptTag() {
+    const props: ScriptLoaderProps = {
+        src: "https://ercaws.com/time",
+        debug: true,
+        renderScript: true,
+        "data-testid": "script-tag-generated-tag",
+        render: (
+            <div data-testid="custom-render">
                 <form data-testid="his-honorable-form">
-                  <label htmlFor="honorable-input-username" >Name:</label>
-                  <input 
-                        title='honorable-input'
-                        name="honorable-input-username" 
-                        type="text" 
-                        data-testid="his-honorable-input" 
+                    <label htmlFor="honorable-input-username">Name:</label>
+                    <input
+                        title="honorable-input"
+                        name="honorable-input-username"
+                        type="text"
+                        data-testid="his-honorable-input"
                         value="Edward Falesetien"
-                  />
+                    />
                 </form>
-               </div>
-  }
+            </div>
+        ),
+    };
 
-  return render(<ScriptTag {...props} />);
+    return render(<ScriptTag {...props} />);
 }
